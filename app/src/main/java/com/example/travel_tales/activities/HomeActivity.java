eb25b555d14db1c192609d215ce8e8b1b8105027
Harshada <hyadav8701@conestogac.on.ca>
2024-04-16 01:11:39 -0400

    Changes in HomeActivity, MainActivity, adding bottom navigation to all pages and navdrawer and fragments

--- app/src/main/java/com/example/travel_tales/activities/HomeActivity.java
+++ app/src/main/java/com/example/travel_tales/activities/HomeActivity.java
@@ -1,19 +1,28 @@
 package com.example.travel_tales.activities;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
-import com.example.travel_tales.activities.JournalActivity;
-import com.example.travel_tales.activities.JournalGalleryActivity;
-import com.example.travel_tales.activities.MapActivity;
+import com.example.travel_tales.R;
+import com.example.travel_tales.fragments.AddJournalFragment;
 import com.example.travel_tales.databinding.ActivityHomeBinding;
-import com.example.travel_tales.databinding.ActivityMainBinding;
+import com.example.travel_tales.fragments.DeleteJournalFragment;
+import com.example.travel_tales.fragments.ListJournalFragment;
+import com.example.travel_tales.fragments.UpdateJournalFragment;
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
     ActivityHomeBinding homeBinding;
+
+    ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +37,44 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         homeBinding.button.setOnClickListener(this);
         homeBinding.mapButton.setOnClickListener(this);
         homeBinding.capturedImages.setOnClickListener(this);
+
+        // creating navigation drawer
+        mToggle = new ActionBarDrawerToggle(this,homeBinding.drawerLayout, homeBinding.materialToolbar, R.string.open, R.string.close);
+        homeBinding.drawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        setSupportActionBar(homeBinding.materialToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        SetNavigationDrawer();
+    }
+
+    // Initializing navigation drawer items to the fragments
+    private void SetNavigationDrawer() {
+        homeBinding.navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment frag = null;
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.nav_add_journal) {
+                    frag = new AddJournalFragment();
+                } else if (itemId == R.id.nav_delete_journal) {
+                    frag = new DeleteJournalFragment();
+                } else if (itemId == R.id.nav_list_journal) {
+                    frag = new ListJournalFragment();
+                } else if (itemId == R.id.nav_update_journal) {
+                    frag = new UpdateJournalFragment();
+                }
+                if (frag != null) {
+                    FragmentTransaction frgTrans = getSupportFragmentManager().beginTransaction();
+                    frgTrans.replace(R.id.frame, frag);
+                    frgTrans.commit();
+                    homeBinding.drawerLayout.closeDrawers();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     @Override
@@ -42,5 +89,13 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
             Intent intent = new Intent(this, JournalGalleryActivity.class);
             startActivity(intent);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        if(mToggle.onOptionsItemSelected(item)){
+            return  true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
